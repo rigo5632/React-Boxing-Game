363a0ee94e87d71fec00cb5a061f0b6e1caeab5e
rigo5632 <rmquiroz5@gmail.com>
2020-05-26 15:30:39 -0600

    deploy

---
+++ precache-manifest.c9b1c4a8e91c8515e78f634a426b6c5d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a75780f9fe4d1043901278f595e73a",
+    "url": "/react-boxing-game/index.html"
+  },
+  {
+    "revision": "2b5ec44020ad6271dfd5",
+    "url": "/react-boxing-game/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "e72d2bb3139d8b6cc921",
+    "url": "/react-boxing-game/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "2b5ec44020ad6271dfd5",
+    "url": "/react-boxing-game/static/js/2.1c369f58.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-boxing-game/static/js/2.1c369f58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e72d2bb3139d8b6cc921",
+    "url": "/react-boxing-game/static/js/main.c65f4dee.chunk.js"
+  },
+  {
+    "revision": "69f5ad38da8fe11ee115",
+    "url": "/react-boxing-game/static/js/runtime-main.3ba3ee89.js"
+  },
+  {
+    "revision": "135414266eb08be3bdf5269053fd6c00",
+    "url": "/react-boxing-game/static/media/bag-burst.13541426.png"
+  }
+]);
